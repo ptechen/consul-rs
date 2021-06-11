8680d941f91fd32542f63bdac6cdbec93189e815
陶辰 <taochen@taochendeMacBook-Pro-2.local>
2021-06-11 15:28:52 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 #[cfg(test)]
 mod tests {
+    use async_std::task::block_on;
+    use crate::set_client_address;
+
     #[test]
     fn it_works() {
+        block_on(set_client_address("123"));
         assert_eq!(2 + 2, 4);
     }
 }
@@ -11,3 +15,9 @@ pub mod health;
 pub mod api;
 pub mod catalog;
 pub mod config_entry;
+
+pub async fn set_client_address(address: &'static str) {
+    api::Client::set_config_address(address).await;
+    health::Health::reload_client().await;
+    agent::Agent::reload_client().await;
+}
